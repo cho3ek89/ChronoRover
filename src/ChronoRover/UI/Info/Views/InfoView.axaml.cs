7f7fc3cbff26e023efe6fb14ea2bd2ffed515c7e
cho3ek89 <kamilek.cholewa@gmail.com>
2026-08-07 12:39:07 +0200

    Fixing the navigation in Info view.

--- src/ChronoRover/UI/Info/Views/InfoView.axaml.cs
+++ src/ChronoRover/UI/Info/Views/InfoView.axaml.cs
@@ -29,27 +29,27 @@ public partial class InfoView : ContentPage
         DataContext = _serviceProvider.GetRequiredService<InfoViewModel>();
     }
 
-    private async void ManualClick(object sender, RoutedEventArgs e)
-    {
+    private async void ManualClick(object sender, RoutedEventArgs e) =>
         await NavigateToView<ManualView>();
-    }
 
     private async void ManualKeyDown(object sender, KeyEventArgs e)
     {
-        if (e.Key == Key.Enter)
+        if (CanNavigateToView(e.Key))
             await NavigateToView<ManualView>();
     }
 
-    private async void AboutClick(object sender, RoutedEventArgs e)
-    {
+    private async void AboutClick(object sender, RoutedEventArgs e) =>
         await NavigateToView<AboutView>();
-    }
 
     private async void AboutKeyDown(object sender, KeyEventArgs e)
     {
-        if (e.Key == Key.Enter)
+        if (CanNavigateToView(e.Key))
             await NavigateToView<AboutView>();
     }
+
+    /// <remarks>Some remote controls map the OK/Select button to Space.</remarks>
+    [SuppressMessage("ReSharper", "PatternIsRedundant")]
+    private static bool CanNavigateToView(Key key) => key is Key.Enter or Key.Return or Key.Space;
 
     private async Task NavigateToView<T>() where T : ContentPage
     {
